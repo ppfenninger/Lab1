28bf620b5e5c3e33670f232626cc84db21048824
lapudvan <lauren.pudvan@gmail.com>
2018-10-02 19:52:58 -0400

    Updating modules

--- alu.v
+++ alu.v
@@ -65,12 +65,11 @@ endmodule
 module anderAndnander
 (
     output res[31:0],
-    input a, 
-    input b,
+    input a[31:0], 
+    input b[31:0],
     input invert //used for nand   if invert=1 then not AandB
 );  // AND gate produces AandB from A and B
-    wire AandB;
-    wire nAandB;
+    wire AandB[31:0];
     `AND  andgate(AandB[0], a[0], b[0]);
     `AND  andgate(AandB[1], a[1], b[1]);
     `AND  andgate(AandB[2], a[2], b[2]);
@@ -104,35 +103,46 @@ module anderAndnander
     `AND  andgate(AandB[30], a[30], b[30]);
     `AND  andgate(AandB[31], a[31], b[31]);
     `XOR  xorgate(res[0], AandB[0], invert);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    `XOR  xorgate(res[1], AandB[1], invert);
+    `XOR  xorgate(res[2], AandB[2], invert);
+    `XOR  xorgate(res[3], AandB[3], invert);
+    `XOR  xorgate(res[4], AandB[4], invert);
+    `XOR  xorgate(res[5], AandB[5], invert);
+    `XOR  xorgate(res[6], AandB[6], invert);
+    `XOR  xorgate(res[7], AandB[7], invert);
+    `XOR  xorgate(res[8], AandB[8], invert);
+    `XOR  xorgate(res[9], AandB[9], invert);
+    `XOR  xorgate(res[10], AandB[10], invert);
+    `XOR  xorgate(res[11], AandB[11], invert);
+    `XOR  xorgate(res[12], AandB[12], invert);
+    `XOR  xorgate(res[13], AandB[13], invert);
+    `XOR  xorgate(res[14], AandB[14], invert);
+    `XOR  xorgate(res[15], AandB[15], invert);
+    `XOR  xorgate(res[16], AandB[16], invert);
+    `XOR  xorgate(res[17], AandB[17], invert);
+    `XOR  xorgate(res[18], AandB[18], invert);
+    `XOR  xorgate(res[19], AandB[19], invert);
+    `XOR  xorgate(res[20], AandB[20], invert);
+    `XOR  xorgate(res[21], AandB[21], invert);
+    `XOR  xorgate(res[22], AandB[22], invert);
+    `XOR  xorgate(res[23], AandB[23], invert);
+    `XOR  xorgate(res[24], AandB[24], invert);
+    `XOR  xorgate(res[25], AandB[25], invert);
+    `XOR  xorgate(res[26], AandB[26], invert);
+    `XOR  xorgate(res[27], AandB[27], invert);
+    `XOR  xorgate(res[28], AandB[28], invert);
+    `XOR  xorgate(res[29], AandB[29], invert);
+    `XOR  xorgate(res[30], AandB[30], invert);
 endmodule
 
 module orer
 (
-    output AorB[31:0],
-    input a, 
-    input b 
+    output res[31:0],
+    input a[31:0], 
+    input b[31:0],
+    input invert 
 );  // OR gate produces AorB from A and B
+    wire AorB[31:0];
     `OR  orgate(AorB[0], a[0], b[0]);
     `OR  orgate(AorB[1], a[1], b[1]);
     `OR  orgate(AorB[2], a[2], b[2]);
@@ -165,56 +175,88 @@ module orer
     `OR  orgate(AorB[29], a[29], b[29]);
     `OR  orgate(AorB[30], a[30], b[30]);
     `OR  orgate(AorB[31], a[31], b[31]);
-endmodule
-
-module norer
-(
-    output AnorB,
-    input a, 
-    input b
-);  // NOR gate produces AonrB from A and B
-    `NOR  norgate(AnorB[0], a[0], b[0]);
-    `NOR  norgate(AnorB[1], a[1], b[1]);
-    `NOR  norgate(AnorB[2], a[2], b[2]);
-    `NOR  norgate(AnorB[3], a[3], b[3]);
-    `NOR  norgate(AnorB[4], a[4], b[4]);
-    `NOR  norgate(AnorB[5], a[5], b[5]);
-    `NOR  norgate(AnorB[6], a[6], b[6]);
-    `NOR  norgate(AnorB[7], a[7], b[7]);
-    `NOR  norgate(AnorB[8], a[8], b[8]);
-    `NOR  norgate(AnorB[9], a[9], b[9]);
-    `NOR  norgate(AnorB[10], a[10], b[10]);
-    `NOR  norgate(AnorB[11], a[11], b[11]);
-    `NOR  norgate(AnorB[12], a[12], b[12]);
-    `NOR  norgate(AnorB[13], a[13], b[13]);
-    `NOR  norgate(AnorB[14], a[14], b[14]);
-    `NOR  norgate(AnorB[15], a[15], b[15]);
-    `NOR  norgate(AnorB[16], a[16], b[16]);
-    `NOR  norgate(AnorB[17], a[17], b[17]);
-    `NOR  norgate(AnorB[18], a[18], b[18]);
-    `NOR  norgate(AnorB[19], a[19], b[19]);
-    `NOR  norgate(AnorB[20], a[20], b[20]);
-    `NOR  norgate(AnorB[21], a[21], b[21]);
-    `NOR  norgate(AnorB[22], a[22], b[22]);
-    `NOR  norgate(AnorB[23], a[23], b[23]);
-    `NOR  norgate(AnorB[24], a[24], b[24]);
-    `NOR  norgate(AnorB[25], a[25], b[25]);
-    `NOR  norgate(AnorB[26], a[26], b[26]);
-    `NOR  norgate(AnorB[27], a[27], b[27]);
-    `NOR  norgate(AnorB[28], a[28], b[28]);
-    `NOR  norgate(AnorB[29], a[29], b[29]);
-    `NOR  norgate(AnorB[30], a[30], b[30]);
-    `NOR  norgate(AnorB[31], a[31], b[31]);
+    `XOR  xorgate(res[0], AorB[0], invert);
+    `XOR  xorgate(res[1], AorB[1], invert);
+    `XOR  xorgate(res[2], AorB[2], invert);
+    `XOR  xorgate(res[3], AorB[3], invert);
+    `XOR  xorgate(res[4], AorB[4], invert);
+    `XOR  xorgate(res[5], AorB[5], invert);
+    `XOR  xorgate(res[6], AorB[6], invert);
+    `XOR  xorgate(res[7], AorB[7], invert);
+    `XOR  xorgate(res[8], AorB[8], invert);
+    `XOR  xorgate(res[9], AorB[9], invert);
+    `XOR  xorgate(res[10], AorB[10], invert);
+    `XOR  xorgate(res[11], AorB[11], invert);
+    `XOR  xorgate(res[12], AorB[12], invert);
+    `XOR  xorgate(res[13], AorB[13], invert);
+    `XOR  xorgate(res[14], AorB[14], invert);
+    `XOR  xorgate(res[15], AorB[15], invert);
+    `XOR  xorgate(res[16], AorB[16], invert);
+    `XOR  xorgate(res[17], AorB[17], invert);
+    `XOR  xorgate(res[18], AorB[18], invert);
+    `XOR  xorgate(res[19], AorB[19], invert);
+    `XOR  xorgate(res[20], AorB[20], invert);
+    `XOR  xorgate(res[21], AorB[21], invert);
+    `XOR  xorgate(res[22], AorB[22], invert);
+    `XOR  xorgate(res[23], AorB[23], invert);
+    `XOR  xorgate(res[24], AorB[24], invert);
+    `XOR  xorgate(res[25], AorB[25], invert);
+    `XOR  xorgate(res[26], AorB[26], invert);
+    `XOR  xorgate(res[27], AorB[27], invert);
+    `XOR  xorgate(res[28], AorB[28], invert);
+    `XOR  xorgate(res[29], AorB[29], invert);
+    `XOR  xorgate(res[30], AorB[30], invert);
 endmodule
 
 module xorer
 (
 	output xorer[31:0],
-	input a,
-	input b
+	input a[31:0],
+	input b[31:0]
 );
 	`XOR  xorgate(xorer[0], a[0], b[0]);
-  `XOR  xorgate(xorer[0], a[0], b[0]);
+  `XOR  xorgate(xorer[1], a[1], b[1]);
+  `XOR  xorgate(xorer[2], a[2], b[2]);
+  `XOR  xorgate(xorer[3], a[3], b[3]);
+  `XOR  xorgate(xorer[4], a[4], b[4]);
+  `XOR  xorgate(xorer[5], a[5], b[5]);
+  `XOR  xorgate(xorer[6], a[6], b[6]);
+  `XOR  xorgate(xorer[7], a[7], b[7]);
+  `XOR  xorgate(xorer[8], a[8], b[8]);
+  `XOR  xorgate(xorer[9], a[9], b[9]);
+  `XOR  xorgate(xorer[10], a[10], b[10]);
+  `XOR  xorgate(xorer[11], a[11], b[11]);
+  `XOR  xorgate(xorer[12], a[12], b[12]);
+  `XOR  xorgate(xorer[13], a[13], b[13]);
+  `XOR  xorgate(xorer[14], a[14], b[14]);
+  `XOR  xorgate(xorer[15], a[15], b[15]);
+  `XOR  xorgate(xorer[16], a[16], b[16]);
+  `XOR  xorgate(xorer[17], a[17], b[17]);
+  `XOR  xorgate(xorer[18], a[18], b[18]);
+  `XOR  xorgate(xorer[19], a[19], b[19]);
+  `XOR  xorgate(xorer[20], a[20], b[20]);
+  `XOR  xorgate(xorer[21], a[21], b[21]);
+  `XOR  xorgate(xorer[22], a[22], b[22]);
+  `XOR  xorgate(xorer[23], a[23], b[23]);
+  `XOR  xorgate(xorer[24], a[24], b[24]);
+  `XOR  xorgate(xorer[25], a[25], b[25]);
+  `XOR  xorgate(xorer[26], a[26], b[26]);
+  `XOR  xorgate(xorer[27], a[27], b[27]);
+  `XOR  xorgate(xorer[28], a[28], b[28]);
+  `XOR  xorgate(xorer[29], a[29], b[29]);
+  `XOR  xorgate(xorer[30], a[30], b[30]);
+  `XOR  xorgate(xorer[31], a[31], b[31]);
+endmodule
+
+module slt
+(
+    output res,
+    input subRes[31:0],
+    input overflow 
+);
+    wire invOverflow;
+    `NOT  invgate(invOverflow, overflow); // inverts overflow
+    `AND  andgate(res, invOverflow, subRes[31]) // if invover and bit1 are high. then slt result is high
 endmodule
 
 module AdderAndSubtractor
@@ -237,17 +279,6 @@ module AdderAndSubtractor
     `AND  andgate(AandB, a, BxorSub);
     `AND  andgate(xAorBandCin, xAorB, carryin);
     `OR   orgate(carryout, AandB, xAorBandCin);
-endmodule
-
-module slt
-(
-    output slter,
-    input subResBit1,
-    input overflow 
-);
-    wire invOverflow;
-    `NOT  invgate(invOverflow, overflow); // inverts overflow
-    `AND  andgate(slter, invOverflow, subResBit1) // if invover and bit1 are high. then slt result is high
 endmodule
 
 module behavioral4bitMultiplexer
