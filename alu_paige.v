d87167830f493c1fee42cc0c498221daa51bf571
lapudvan <lauren.pudvan@gmail.com>
2018-10-03 21:10:33 -0400

    with gate delays

--- alu_paige.v
+++ alu_paige.v
@@ -1,9 +1,13 @@
-`define AND and #5
-`define OR or #5
-`define XOR xor #5
-`define NOT not #5
-`define NAND nand #5
-`define NOR nor #5
+`define AND2 and #40
+`define AND3 and #60
+`define AND4 and #80
+`define OR2 or #40
+`define OR8 or #160
+`define XOR2 xor #40
+`define NOT1 not #10
+`define NAND2 nand #20
+`define NOR2 nor #20
+`define NOR32 nor #320
 
 module didOverflow
 (
@@ -21,15 +25,15 @@ module didOverflow
     wire notaAndNotb;
     wire negToPos;
     wire posToNeg;
-    `XOR xorgate(BxorSub, b, sub);
-    `NOT aNot(notA, a);
-    `NOT bNot(notB, BxorSub);
-    `NOT sNot(notS, s);
-    `AND andab(aAndB, a, BxorSub);
-    `AND andabNot(notaAndNotb, notA, notB);
-    `AND andSwitch1(negToPos, aAndB, notS);
-    `AND andSwitch2(posToNeg, notaAndNotb, s);
-    `OR orGate(overflow, negToPos, posToNeg);
+    `XOR2 xorgate(BxorSub, b, sub);
+    `NOT1 aNot(notA, a);
+    `NOT1 bNot(notB, BxorSub);
+    `NOT1 sNot(notS, s);
+    `AND2 andab(aAndB, a, BxorSub);
+    `AND2 andabNot(notaAndNotb, notA, notB);
+    `AND2 andSwitch1(negToPos, aAndB, notS);
+    `AND2 andSwitch2(posToNeg, notaAndNotb, s);
+    `OR2 orGate(overflow, negToPos, posToNeg);
 endmodule
 
 module AdderAndSubtractor
@@ -45,12 +49,12 @@ module AdderAndSubtractor
     wire xAorB;
     wire AandB;
     wire xAorBandCin;
-    `XOR  xorgate(BxorSub, b, isSubtract);
-    `XOR  xorgate(xAorB, a, BxorSub);   // OR gate produces AorB from A and B
-    `XOR  xorgate(res, xAorB, carryin);
-    `AND  andgate(AandB, a, BxorSub);
-    `AND  andgate(xAorBandCin, xAorB, carryin);
-    `OR   orgate(carryout, AandB, xAorBandCin);
+    `XOR2  xorgate(BxorSub, b, isSubtract);
+    `XOR2  xorgate(xAorB, a, BxorSub);   // OR gate produces AorB from A and B
+    `XOR2  xorgate(res, xAorB, carryin);
+    `AND2  andgate(AandB, a, BxorSub);
+    `AND2  andgate(xAorBandCin, xAorB, carryin);
+    `OR2   orgate(carryout, AandB, xAorBandCin);
 endmodule
 
 module aluBitSlice
@@ -86,11 +90,11 @@ module aluBitSlice
     wire isNand;
     wire isSLT;
 
-    `AND(andRes, a, b);
-    `NAND(nandRes, a, b);
-    `OR(orRes, a, b);
-    `NOR(norRes, a, b);
-    `XOR(xorRes, a, b);
+    `AND2(andRes, a, b);
+    `NAND2(nandRes, a, b);
+    `OR2(orRes, a, b);
+    `NOR2(norRes, a, b);
+    `XOR2(xorRes, a, b);
 
     AdderAndSubtractor adder (
         .res (addSub),
@@ -101,20 +105,20 @@ module aluBitSlice
         .carryin (carryIn)
     );
 
-    `NOT(s0inv, s0);
-    `NOT(s1inv, s1);
-    `NOT(s2inv, s2);
+    `NOT1(s0inv, s0);
+    `NOT1(s1inv, s1);
+    `NOT1(s2inv, s2);
 
-    `AND(isAdd, addSub, s0inv, s1inv, s2inv);
-    `AND(isSub, addSub, s0, s1inv, s2inv);
-    `AND(isXor, xorRes, s0inv, s1, s2inv);
-    `AND(isSLT, addSub, s0, s1, s2inv);
-    `AND(isAnd, andRes, s0inv, s1inv, s2);
-    `AND(isNand, nandRes, s0, s1inv, s2);
-    `AND(isNor, norRes, s0inv, s1, s2);
-    `AND(isOr, orRes, s0, s1, s2);
+    `AND4(isAdd, addSub, s0inv, s1inv, s2inv);
+    `AND4(isSub, addSub, s0, s1inv, s2inv);
+    `AND4(isXor, xorRes, s0inv, s1, s2inv);
+    `AND4(isSLT, addSub, s0, s1, s2inv);
+    `AND4(isAnd, andRes, s0inv, s1inv, s2);
+    `AND4(isNand, nandRes, s0, s1inv, s2);
+    `AND4(isNor, norRes, s0inv, s1, s2);
+    `AND4(isOr, orRes, s0, s1, s2);
 
-    `OR(initialResult, isAdd, isSub, isXor, isAnd, isNand, isNor, isOr, isSLT);
+    `OR8(initialResult, isAdd, isSub, isXor, isAnd, isNand, isNor, isOr, isSLT);
 
 endmodule
 
@@ -123,7 +127,7 @@ module isZero (
     output out
 );
 
-`NOR(out, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], 
+`NOR32(out, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], 
     a[15], a[16], a[17], a[18], a[19], a[20], a[21], a[22], a[23], a[24], a[25], a[26], a[27], a[28], a[29], a[30], a[31]);
 
 endmodule // isZero
@@ -142,8 +146,8 @@ module alu (
     wire[32:0] carryOut;
     wire isSubtract;
 
-    `OR(isSubtract, command[0], command[0]);
-    `OR(carryOut[0], isSubtract, isSubtract); 
+    `OR2(isSubtract, command[0], command[0]);
+    `OR2(carryOut[0], isSubtract, isSubtract); 
 
 
     generate
@@ -164,7 +168,7 @@ module alu (
         end
     endgenerate
 
-    `OR(carryout, carryOut[32], carryOut[32]);
+    `OR2(carryout, carryOut[32], carryOut[32]);
 
     didOverflow overflowCalc(
         .overflow (overflow),
@@ -180,21 +184,21 @@ module alu (
     wire isSLT;
     wire SLTval;
 
-    `NOT(s2inv, command[2]);
-    `NOT(overflowInv, overflow);
-    `AND(isSLT, s2inv, command[0], command[1]);
-    `NOT(isSLTinv, isSLT);
-    `AND(SLTval, initialResult[31], overflowInv, isSLT);
+    `NOT1(s2inv, command[2]);
+    `NOT1(overflowInv, overflow);
+    `AND3(isSLT, s2inv, command[0], command[1]);
+    `NOT1(isSLTinv, isSLT);
+    `AND3(SLTval, initialResult[31], overflowInv, isSLT);
 
     generate
         genvar j;
         for (j=1; j<32; j=j+1)
         begin
-            `AND(result[j], initialResult[j], isSLTinv);
+            `AND2(result[j], initialResult[j], isSLTinv);
         end
     endgenerate
 
-    `OR(result[0], initialResult[0], SLTval);
+    `OR2(result[0], initialResult[0], SLTval);
 
     isZero zeroCalc(
         .a (result),
